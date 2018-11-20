878a16c02f7ca87d1f3289ef20aff4c7b64647a0
jfeo <jensfeodor@gmail.com>
2018-11-20 15:27:26 +0100

    Move global datastructures to mysql database.

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,3 +4,12 @@ CREATE TABLE swipes (
 	choice CHAR(10),
 	PRIMARY KEY (user_uuid, asset_id)
 );
+
+CREATE TABLE assets (
+	id CHAR(64),
+	title TEXT,
+	thumb CHAR(128),
+	upvotes INT,
+	downvotes INT,
+	PRIMARY KEY (id)
+);
